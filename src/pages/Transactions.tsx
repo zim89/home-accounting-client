d78281d987bc1118028ed4babf6693a652dadafe
zim89 <121241841+zim89@users.noreply.github.com>
2023-08-22 10:54:10 +0300

    edit transactions.tsx

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,56 +1,10 @@
 import { FC } from 'react';
 import TransactionForm from '../components/TransactionForm';
-import {
-  ICategory,
-  IResponseTransactionsLoader,
-  ITransaction,
-} from '../types/types';
-import { instance } from '../api/axios.api';
-import { toast } from 'react-toastify';
+import { IResponseTransactionsLoader } from '../types/types';
 import TransactionTable from '../components/TransactionTable';
 import { useLoaderData } from 'react-router-dom';
 import { formatToUSD } from '../helpers/currency.helper';
 import Chart from '../components/Chart';
-
-// export const transactionsLoader = async () => {
-//   const categories = await instance.get<ICategory[]>('/categories');
-//   const transactions = await instance.get<ITransaction[]>('/transactions');
-//   const totalIncome = await instance.get<number>('/transactions/income');
-//   const totalExpense = await instance.get<number>('/transactions/expense');
-
-//   const data = {
-//     categories: categories.data,
-//     transactions: transactions.data,
-//     totalIncome: totalIncome.data,
-//     totalExpense: totalExpense.data,
-//   };
-//   return data;
-// };
-
-// export const transactionsAction = async ({ request }: any) => {
-//   switch (request.method) {
-//     case 'POST': {
-//       const formData = await request.formData();
-//       const newTransaction = {
-//         title: formData.get('title'),
-//         amount: +formData.get('amount'),
-//         category: formData.get('category'),
-//         type: formData.get('type'),
-//       };
-//       await instance.post('/transactions', newTransaction);
-//       toast.success('Transaction success added.');
-//       return null;
-//     }
-
-//     case 'DELETE': {
-//       const formData = await request.formData();
-//       const transactionId = formData.get('id');
-//       await instance.delete(`/transactions/transaction/${transactionId}`);
-//       toast.success('Transaction success deleted. ');
-//       return null;
-//     }
-//   }
-// };
 
 const Transactions: FC = () => {
   const { totalIncome, totalExpense } =
